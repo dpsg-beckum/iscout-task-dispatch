9649c8c3e06fa83496d1a9a41b9e138e07d26136
Jonah Wille <jonah.wille10@gmail.com>
2023-11-20 07:46:43 +0100

    implementet Task and Team calls

--- iscouttaskdispatch/api/api.py
+++ iscouttaskdispatch/api/api.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, Response, abort, jsonify
+from ..database.db import db
+from ..database.json_encoder import DatabaseEncoder
+from ..database.db import Team, Task, TaskHasStatus, Status
 
 api_routes = Blueprint("api", __name__, template_folder="templates",
                        url_prefix="")
@@ -7,17 +9,74 @@ api_routes = Blueprint("api", __name__, template_folder="templates",
 
 
 @api_routes.route("/task", methods=['GET'])
-def getAllTasks():
-    return "Task"
+def AllTasks():
+    result = [DatabaseEncoder.default(i) for i in Task.query.all()]
+    return jsonify(result)
 
 @api_routes.route("/task/<taskID>", methods=['GET','POST','PUT','DELETE'])
-def getTask(taskID):
-    return "Task via ID"
+def task(taskID):
+    if request.method == 'GET':
+        result = DatabaseEncoder.default(Task.query.get(taskID))
+        print(result)
+        if result == None:
+            abort(404, "Task Not Found")
+        else:
+            return result
+    if request.method == 'POST':
+        if not request.args.get("name") or not request.args.get("description"):
+            abort(400)
+        if not Task.query.get(taskID):
+            name = str(request.args.get("name")).replace('"','')
+            description = str(request.args.get("description")).replace('"','')
+            db.session.add(Task(taskID = taskID, name=name, description=description))
+            db.session.commit()
+            return DatabaseEncoder.default(Task.query.get(taskID))
+        else:
+            abort(409)
+    if request.method == 'DELETE':
+        if Task.query.get(taskID):
+            x = db.session.query(Task).get(taskID)
+            db.session.delete(x)
+            db.session.commit()
+            
+            return "OK"
+        else:
+            abort(400)
 
 @api_routes.route("/team", methods=['GET'])
-def getAllTeams():
-    return "Team"
+def AllTeams():
+    result = [DatabaseEncoder.default(i) for i in Team.query.all()]
+    return jsonify(result)
+
 
 @api_routes.route("/team/<teamID>", methods=['GET','POST','PUT','DELETE'])
-def getTeam(teamID):
-    return "Team via ID"
+def team(teamID):
+    if request.method == 'GET':
+        result = DatabaseEncoder.default(Team.query.get(teamID))
+        if result == None:
+            abort(404, "Team Not Found")
+        else:
+            return result
+    if request.method == 'POST':
+        if not request.args.get("name"):
+            abort(400)
+        if not Team.query.get(teamID):
+            db.session.add(Team(teamID = teamID, name=str(request.args.get("name"))))
+            db.session.commit()
+            return DatabaseEncoder.default(Team.query.get(teamID))
+        else:
+            abort(409)
+    if request.method == 'DELETE':
+        if Team.query.get(teamID):
+            x = db.session.query(Team).get(teamID)
+            db.session.delete(x)
+            db.session.commit()
+            
+            return "OK"
+        else:
+            abort(400)
+
+@api_routes.route("/assignTask/<taskID>", methods=['PUT'])
+def assignTask(taskID):
+
+    pass
