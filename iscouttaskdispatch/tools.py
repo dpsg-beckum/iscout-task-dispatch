f9dc8388475d8609605a7d04d43eba201f7b2e75
Jonah Wille <jonah.wille10@gmail.com>
2023-11-22 11:40:10 +0100

    changed time format

--- iscouttaskdispatch/tools.py
+++ iscouttaskdispatch/tools.py
@@ -2,4 +2,4 @@ from datetime import datetime as dt
 
 
 def formatDatetime(timestamp : dt) -> str:
-    return timestamp.strftime("%H:%M:%S") if timestamp.date() == dt.today().date() else timestamp.strftime("%m/%d/%Y, %H:%M:%S")
+    return timestamp.strftime("%H:%M:%S") if timestamp.date() == dt.today().date() else timestamp.strftime("%d.%m.%Y, %H:%M:%S")
