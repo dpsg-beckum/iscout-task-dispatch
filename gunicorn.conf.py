b74b73804bd65d5f349d773001b06d1be49f5578
Jonah <jonah.wille10@gmail.com>
2024-03-01 16:20:46 +0000

    removed acess log

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,7 @@ import multiprocessing
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
-accesslog = "acess.log"
+# accesslog = "acess.log"
 # Use gevent for async workers, if your app is I/O bound
 # worker_class = 'gevent'
 
