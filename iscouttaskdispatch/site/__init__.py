00f7fa07529456e65380c521f1396b05bc9e2e55
Jonah Wille <jonah.wille10@gmail.com>
2025-02-28 14:38:08 +0100

    feat(error-handling): add custom error handler for ElementDoesNotExsist and enhance error page layout

--- iscouttaskdispatch/site/__init__.py
+++ iscouttaskdispatch/site/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
 from ..database.db import Team
+from ..database.exceptions import ElementDoesNotExsist
 from .views import overview, spielleitung, team
 
 site = Blueprint("site", __name__, template_folder="templates")
@@ -9,6 +10,11 @@ site = Blueprint("site", __name__, template_folder="templates")
 site.register_blueprint(spielleitung.spielleitung_site)
 site.register_blueprint(team.teams_site)
 site.register_blueprint(overview.overview_site)
+
+
+@site.errorhandler(ElementDoesNotExsist)
+def handle_element_does_not_exist(error):
+    return render_template("error.html", error=error, code=404), 404
 
 
 @site.after_request
